932b89776c64407afcd4653902069e7dba8117cf
HeberthDev <heberthdeveloper@gmail.com>
2024-07-23 07:57:04 -0300

    Lista filtrada

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -6,8 +6,13 @@ async function buscarVideo(evento) {
 
     const dadosDePesquisa = document.querySelector('[data-pesquisa]').value;
     const busca = await conectaAPI.buscaVideo(dadosDePesquisa);
-
     const lista = document.querySelector('[data-lista]');
+
+    //Enquanto tiver 1 elemento vai remover até ficar em zero e posteriormente adicionarmos apenas o que veio da busca
+    while (lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+
     busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
 }
 
