2fbfc20631935608fd45e842c8c3c61c014ca734
HeberthDev <heberthdeveloper@gmail.com>
2024-07-23 07:53:12 -0300

    Reutilizar funções

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -1,4 +1,5 @@
 import { conectaAPI } from "./conectaAPI.js";
+import constroiCard from "./mostrarVideos.js";
 
 async function buscarVideo(evento) {
     evento.preventDefault();
@@ -6,7 +7,8 @@ async function buscarVideo(evento) {
     const dadosDePesquisa = document.querySelector('[data-pesquisa]').value;
     const busca = await conectaAPI.buscaVideo(dadosDePesquisa);
 
-    console.log(busca);
+    const lista = document.querySelector('[data-lista]');
+    busca.forEach(elemento => lista.appendChild(constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)));
 }
 
 const botaoDePesquisa = document.querySelector('[data-botao-pesquisa]');
